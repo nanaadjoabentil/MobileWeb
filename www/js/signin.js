675063ad9fe0b678a994301ae8fe8322b093424a
nanaadjoabentil <nana.bentil@outlook.com>
2017-12-09 23:15:13 +0000

    11:15 dec 9

--- www/js/signin.js
+++ www/js/signin.js
@@ -1,27 +1,28 @@
 function signin()
 {
-  var username = document.getElementById('username').value;
-  var password = document.getElementById('password').value;
-
-  var info = 'username='+username + '&password='+password;
-
-  if (username == "" || password = "")
-  {
-    alert("Please fill all fields");
-  }
-  else {
-    $.ajax({
-      type: "POST",
-      url: "signin.php",
-      data{
-        username: username,
-        password: password
-      },
-      cache: false,
-      success: function(html) {
-        alert(html);
-      }
-    });
-  }
-  return false;
+  // var username = document.getElementById('username').value;
+  // var password = document.getElementById('password').value;
+  //
+  // var info = 'username='+username + '&password='+password;
+  //
+  // if (username == "" || password = "")
+  // {
+  //   alert("Please fill all fields");
+  // }
+  // else {
+  //   $.ajax({
+  //     type: "POST",
+  //     url: "signin.php",
+  //     data{
+  //       username: username,
+  //       password: password
+  //     },
+  //     cache: false,
+  //     success: function(html) {
+  //       alert(html);
+  //     }
+  //   });
+  // }
+  // return false;
+  alert();
 }
